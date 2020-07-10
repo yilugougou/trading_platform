85d816b7b502993227e122cfcba3523bd396a732
richard <richard.yao@foxmail.com>
2020-07-10 09:49:42 +0800

    first commit

---
+++ src/components/MyCircle.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="circle">
+    <div class="text">
+      {{msg}}
+    </div>
+    <div class="image">
+      <img src="../assets/logo.png" style="width: 50px;height: 50px;" />
+      <img src="../assets/logo.png" style="width: 50px;height: 50px;" />
+      <img src="../assets/logo.png" style="width: 50px;height: 50px;" />
+    </div>
+    <div class="more">
+
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyCircle',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <MyCircle msg="Welcome to My Circle."/>
+  </div>
+</template>
+
+<script>
+import MyCircle from '@/components/MyCircle.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    MyCircle
+  }
+}
+</script>
+<style>
+  body {
+    margin: 0px;
+  }
+</style>
